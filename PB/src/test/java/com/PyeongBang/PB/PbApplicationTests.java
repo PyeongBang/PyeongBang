7cd660ed88605a4b6709b4ba4589e4baa3fcbf1b
백대현 <eogus6512@gmail.com>
2021-11-03 20:35:43 +0900

    create PyeongBang project

---
+++ PB/src/test/java/com/PyeongBang/PB/PbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PyeongBang.PB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
