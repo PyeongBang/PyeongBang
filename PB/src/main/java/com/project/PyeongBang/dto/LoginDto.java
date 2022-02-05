62066df4539026b67c5cb88cd05acff92c8d549b
백대현 <eogus6512@gmail.com>
2022-02-05 18:19:33 +0900

    swagger관련 apiModel 어노테이션 기능 추가

--- PB/src/main/java/com/project/PyeongBang/dto/LoginDto.java
+++ PB/src/main/java/com/project/PyeongBang/dto/LoginDto.java
@@ -1,5 +1,7 @@
 package com.project.PyeongBang.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
@@ -8,9 +10,13 @@ import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
+@ApiModel(value="로그인", description = "아이디와 패스워드를 가진 Domain")
 public class LoginDto {
+    @ApiModelProperty(value="아이디")
     @NotEmpty(message = "id를 입력해주세요")
     private String id;
+
+    @ApiModelProperty(value="패스워드")
     @Length(min = 3, message = "비밀번호는 3자리 이상 입력해주세요")
     private String pwd;
 
